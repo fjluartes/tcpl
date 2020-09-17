4881a9ddc7db1e2bd2e524b9e29d630658399bb9
Fred <fjluartes@gmail.com>
2020-09-17 21:37:48 +0800

    Adds ex1-15

---
+++ ch1/ex1-15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+float convert(float n);
+
+/* Ex.1-15: Temperature conversion function */
+int main()
+{
+    float i;
+    for (i = 0.0; i <= 300.0; i = i + 20.0)
+        printf("%3.0f %6.1f\n", i, convert(i));
+    return 0;
+}
+
+float convert(float fahr)
+{
+    float celsius = (5.0 / 9.0) * (fahr - 32.0);
+    return celsius;
+}
