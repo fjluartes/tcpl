dc98aa8e5f46ada9106628abd3c8c18e7d88348b
Fred <fjluartes@gmail.com>
2020-09-23 14:45:25 +0800

    Edits ex1-21

--- ch1/ex1-21.c
+++ ch1/ex1-21.c
@@ -4,12 +4,32 @@
 /* Ex. 1-21: Replace spaces with tabs */
 int main()
 {
-    int c, ns, nt, pos;
-    ns = 0;
+    int c, nb, nt, pos;
+    nb = 0;
     nt = 0;
-    while((c = getchar()) != EOF) {
+    for(pos = 1; (c = getchar()) != EOF; ++pos) {
         if (c == ' ') {
-            ++ns;
+            if ((pos % TABSTOP) != 0)
+                ++nb;
+            else {
+                nb = 0;
+                ++nt;
+            }
+        }
+        else {
+            for ( ; nt > 0; --nt)
+                putchar('\t');
+            if (c == '\t')
+                nb = 0;
+            else
+                for ( ; nb > 0; --nb)
+                    putchar(' ');
+            putchar(c);
+            if (c == '\n')
+                pos = 0;
+            else if (c == '\t')
+                pos = pos + (TABSTOP - (pos - 1) % TABSTOP) - 1;
         }
     }
+    return 0;
 }
